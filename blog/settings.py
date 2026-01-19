01ce982dbaa7171865e0377255353ca70f2d3d59
Timoshax <temirlanerkin12@gmail.com>
2026-01-19 20:59:47 +0500

    update database user

--- blog/settings.py
+++ blog/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'blog_july',
-        'USER': 'postgres',
+        'USER': 'tima',
         'PASSWORD': '00000000',
         'HOST': 'localhost',
         'PORT': '5432'
